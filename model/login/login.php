e507776842ba9dc90668b5db242fca20817c2c5a
Mundovirtual <38699384+Mundovirtual@users.noreply.github.com>
2018-12-08 14:14:47 -0600

    regreso

--- model/login/login.php
+++ model/login/login.php
@@ -1,10 +1,10 @@
 <?php 
  
 require_once("../../class/administrador_controller.php");
-$msj="";
-		$aux="0";
+
 	if (isset($_POST['usuario']) and isset($_POST['pass'])) {
-		
+		$msj="";
+		$aux="0";
 
 
 		 $login=new admin();
@@ -21,7 +21,7 @@ $msj="";
 				$_SESSION['id'] =$validar[0];
 				$_SESSION['usr'] = "06222018";  
 		 }
-		 
+		 echo  json_encode(array( 'aux'=> $aux,'msj'=>$msj)); 
 	}
-echo  json_encode(array( 'aux'=> $aux,'msj'=>$msj)); 
+
  ?>
